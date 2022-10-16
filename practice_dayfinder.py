f3b802e66ae1b6c5fd8fbbcd0a2c7e56cd358a44
Vipin Kumar <vkprajapati665@gmail.com>
2022-10-16 22:38:43 +0530

    DayFinder

---
+++ practice_dayfinder.py
@@ -0,0 +1,18 @@
+weekday = int(input("enter week day:"))
+if weekday == 1:
+    print("sunday")
+elif weekday ==2:
+    print("monday")    
+elif weekday ==3:
+    print("tuesday")    
+elif weekday ==4:
+    print("wednesday")    
+elif weekday ==5:
+    print("thurday")    
+elif weekday ==6:
+    print("friday")    
+elif weekday ==7:
+    print("saturaday")    
+elif weekday >7 and weekday < 1:
+    print("Enter valid Day")    
+ 
