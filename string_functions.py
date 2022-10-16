3d11ac5de0268ca4675702bebee9e955bc0c231b
Vipin Kumar <vkprajapati665@gmail.com>
2022-10-16 22:37:55 +0530

    StringFunctions

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+
+
+story="once upon a time in India....."
+print(len(story))
+print(story.endswith("India"))
+print(story.count("n"))
+print(story.capitalize())
+print(story.find("upon"))
+print(story.replace("a","NAme"))
