218d8390e4e33794d1b60b3f44ab45818c4c632e
Vipin Kumar <vkprajapati665@gmail.com>
2022-10-17 21:48:46 +0530

    if_else

---
+++ if_else.py
@@ -0,0 +1,30 @@
+# x = float(input("First no.:"))
+# y = float(input("Seconf no.:"))
+
+# if xy:
+#     print("YAYY")
+# else:
+#     print("NAYY")    
+    
+# # x = 4
+# # y = 5
+# # if x<y:
+# #     print("YAYY")
+# # else:
+# #     print("NAYY")
+
+# length = float(input("Length:"))
+# breadth = float(input("Breadth:"))
+
+# if length == breadth:
+#     print("Its square")
+# else:
+#     print("Its not a square")
+    
+# x = float(input("Salary:"))
+# y = float(input("service years:"))
+
+# if y > 5:
+#     print("Net bonus:", x + 1000)
+# else:
+#     print("Net bonus:", 0)    
